11ad9e7565f261c40a03f47f618da4e2b2882e90
Alex Neves <3845319+alexjneves@users.noreply.github.com>
2023-12-07 20:55:40 +0000

    Day 1 part 1

--- src/day1/day1.rs
+++ src/day1/day1.rs
@@ -8,10 +8,29 @@ impl Day for Day1 {
     fn run(&self, input_type: InputType) -> i32 {
         let input = read_day_input(DAY_ID, input_type);
 
-        println!("Day 1 input: {:?}", input);
-
-        return 0;
+        input.iter()
+            .map(|line| get_calibration_value(line))
+            .sum()
     }
+}
+
+fn get_calibration_value(input: &str) -> i32 {
+    let mut first: Option<i32> = None;
+    let mut last: Option<i32> = None;
+
+    for character in input.chars() {
+        if let Some(digit) = character.to_digit(10) {
+            let digit_int = Some(i32::try_from(digit).unwrap());
+
+            if first.is_none() {
+                first = digit_int;
+            }
+
+            last = digit_int;
+        }
+    }
+
+    first.unwrap() * 10 + last.unwrap()
 }
 
 #[cfg(test)]
@@ -24,6 +43,16 @@ mod tests {
 
         let day1 = Day1 {};
         let answer = day1.run(InputType::Example);
+
+        assert!(answer == EXPECTED_ANSWER);
+    }
+
+    #[test]
+    fn day1_custom_input() {
+        const EXPECTED_ANSWER: i32 = 55607;
+
+        let day1 = Day1 {};
+        let answer = day1.run(InputType::Custom);
 
         assert!(answer == EXPECTED_ANSWER);
     }
